1fe00df940e62452944b36a68777cd04894a80d0
jacopo_farina <jacopo.farina@spindox.it>
2015-07-01 18:26:39 +0200

    basic express structure to serve static files

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 'use strict';
 var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+app.use(express.static('/static'));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
