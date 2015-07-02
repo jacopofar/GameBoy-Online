02f39094f5cacb82b2e87ab5852ad5efb21a6052
jacopo_farina <jacopo.farina@spindox.it>
2015-07-02 23:29:53 +0200

    initial load from the local FS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+var fs = require('fs');
 app.use(express.static('./static'));
 
 var server = app.listen(3000, function () {
@@ -9,10 +10,14 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('GameBoy server listening at http://%s:%s', host, port);
+
   app.ws('/gameboy', function(ws, req) {
     ws.on('message', function(msg) {
-     console.log(msg);
+      console.log(new Date().toISOString() + msg);
     });
+    var initialROM = fs.readFileSync('initrom.gbc', {encoding: 'base64'});
+    console.log("read " + initialROM.length + " base64-encoded bytes from the initial ROM");
+    ws.send(JSON.stringify({command: 'load ROM', romData: initialROM}));
   });
 });
