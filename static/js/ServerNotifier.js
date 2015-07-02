47ce3133b05d4ab0272e578d1975cae654a473e9
jacopo_farina <jacopo.farina@spindox.it>
2015-07-02 22:34:41 +0200

    ROM load can be triggered by the server

--- static/js/ServerNotifier.js
+++ static/js/ServerNotifier.js
@@ -4,3 +4,14 @@ var webSocket = new WebSocket("ws://localhost:3000/gameboy");
 function serverNotify(event){
   webSocket.send(JSON.stringify(event));
 }
+
+webSocket.onmessage = function (event) {
+  var msg = JSON.parse(event.data);
+  if (msg.command === 'load ROM') {
+    var mainCanvas = document.getElementById("mainCanvas");
+    initPlayer();
+    start(mainCanvas, base64_decode(msg.romData));
+  }
+}
+
+
