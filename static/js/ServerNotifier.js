9ae9d92a01c2b25f1c3a7f067a8451dc0c482378
jacopo_farina <jacopo.farina@spindox.it>
2015-07-03 23:49:28 +0200

    use the current hostname, not hardcoded localhost

--- static/js/ServerNotifier.js
+++ static/js/ServerNotifier.js
@@ -1,5 +1,5 @@
 'use strict';
-var webSocket = new WebSocket("ws://localhost:3000/gameboy");
+var webSocket = new WebSocket("ws://" + location.host + "/gameboy");
 
 function serverNotify(event){
   webSocket.send(JSON.stringify(event));
