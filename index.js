7723719bbf78ca5031ec4ba12d5c77f835e6b57c
jacopo_farina <jacopo.farina@spindox.it>
2015-07-04 22:25:23 +0200

    receive commands from an IRC room

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var app = express();
 var expressWs = require('express-ws')(app);
 var fs = require('fs');
 app.use(express.static('./static'));
+var irc    = require('irc');
+var config = require('./config.js');
+
+
+
 fs.open('play_log.txt', 'a',function(err,fd_log){
   if(err){
     console.log("cannot open the log file!");
@@ -16,11 +21,34 @@ fs.open('play_log.txt', 'a',function(err,fd_log){
 
     console.log('GameBoy server listening at http://%s:%s', host, port);
 
+    var ircClient         = new irc.Client(config.server, config.irc_nick, {
+      channels: [config.irc_channel]
+    });
+
+    ircClient.addListener('message', function (from, channel, message) {
+      fs.write(fd_log, JSON.stringify({message: message, from: from, ts: Date.now()}) + "\n");
+      if (message.toLowerCase() === '!help') {
+        ircClient.say(config.irc_channel, 'See the game at ' + config.host_address + ':3000/game_img Available moves: "right", "left", "up", "down", "a", "b", "select", "start"');
+      }
+
+      if (["right", "left", "up", "down", "a", "b", "select", "start"].indexOf(message.toLowerCase()) !== -1) {
+        expressWs.getWss('/gameboy').clients.forEach(function (client) {
+          client.send(JSON.stringify({command: "pressOnce", key: message}));
+        });
+      }
+
+    });
+
+    ircClient.addListener('error', function (message) {
+      console.log('error: ', message);
+    });
+    var latestSavedCanvas = '';
     app.ws('/gameboy', function(ws, req) {
       ws.on('message', function (msg_raw) {
         var msg = JSON.parse(msg_raw);
 
         if (msg.type === 'canvas') {
+          latestSavedCanvas = 'canvas_' + msg.ts + '.png';
           fs.writeFile('canvas_' + msg.ts + '.png', msg.content.replace(/^data:image\/png;base64,/, ""), {encoding: 'base64'}, function (err) {
             if (err)
               console.log("error saving screenshot");
@@ -38,6 +66,11 @@ fs.open('play_log.txt', 'a',function(err,fd_log){
       console.log("read " + initialROM.length + " base64-encoded bytes from the initial ROM");
       ws.send(JSON.stringify({command: 'load ROM', romData: initialROM}));
     });
+
+    app.get('/game_img', function (req, res) {
+      res.sendFile(process.cwd() + '/' + latestSavedCanvas);
+    });
+
   });
 });
 
